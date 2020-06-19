8023da333319c71cee374bd01253c93a8caba9bc
Bo Kehlet <faktor3@gmail.com>
2020-06-19 22:29:39 +0200

    Small things on Afflictions

--- module/dialog/actor-modifiers.js
+++ module/dialog/actor-modifiers.js
@@ -58,6 +58,29 @@ export class DLActorModifiers extends FormApplication {
             html.find(`input[type=checkbox][id="active"]`).prop('checked', false);
         });
 
+        html.find('.radioblockfast').click(ev => {
+            const slowed = html.find(`input[type=checkbox][id="data.afflictions.slowed"]`)[0];
+            const frightened = html.find(`input[type=checkbox][id="data.afflictions.frightened"]`)[0];
+            if (slowed.checked || frightened.checked) {
+                html.find(`input[type=radio][id="data.fastturn.false"]`).prop('checked', true);
+            }
+            else {
+                this.updateTurnOrder(true);
+            }
+        });
+
+        html.find('.radioblock').click(ev => {
+            this.updateTurnOrder(false);
+        });
+
+        // Affliction Rules
+        html.find(`input[type=checkbox][id="data.afflictions.asleep"]`).click(ev => {
+            if (ev.currentTarget.checked) {
+                const prone = html.find(`input[type=checkbox][id="data.afflictions.prone"]`).prop('checked', true);
+                const unconscious = html.find(`input[type=checkbox][id="data.afflictions.unconscious"]`).prop('checked', true);
+            }
+        });
+
         html.find(`input[type=checkbox][id="data.afflictions.frightened"]`).click(ev => {
             if (ev.currentTarget.checked) {
                 html.find(`input[type=radio][id="data.fastturn.false"]`).prop('checked', true);
@@ -65,12 +88,19 @@ export class DLActorModifiers extends FormApplication {
             }
         });
 
-        html.find('.radioblockfast').click(ev => {
-            this.updateTurnOrder(true);
+
+        html.find(`input[type=checkbox][id="data.afflictions.frightened"]`).click(ev => {
+            if (ev.currentTarget.checked) {
+                html.find(`input[type=radio][id="data.fastturn.false"]`).prop('checked', true);
+                this.updateTurnOrder(false);
+            }
         });
 
-        html.find('.radioblock').click(ev => {
-            this.updateTurnOrder(false);
+        html.find(`input[type=checkbox][id="data.afflictions.slowed"]`).click(ev => {
+            if (ev.currentTarget.checked) {
+                html.find(`input[type=radio][id="data.fastturn.false"]`).prop('checked', true);
+                this.updateTurnOrder(false);
+            }
         });
     }
 
