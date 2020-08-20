e323bc500c1bd6acc15ef0646ed35ca55912db91
Bo Kehlet <faktor3@gmail.com>
2020-08-20 22:31:10 +0200

    Dice So Nice custom stuff

--- module/demonlord.js
+++ module/demonlord.js
@@ -388,6 +388,37 @@ Hooks.on("renderChatMessage", async (app, html, msg) => {
     }
 });
 
+Hooks.once('diceSoNiceReady', (dice3d) => {
+    dice3d.addSystem({ id: "demonlord", name: "Demonlord" }, true);
+    dice3d.addDicePreset({
+        type: "d6",
+        labels: [
+            "1", "2", "3", "4", "5",
+            "systems/demonlord/ui/icons/logo.png"
+        ],
+        system: "demonlord"
+    });
+    dice3d.addDicePreset({
+        type: "d20",
+        labels: [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+            "systems/demonlord/ui/icons/logo.png"
+        ],
+        system: "demonlord"
+    });
+    dice3d.addColorset({
+        name: 'demonlord',
+        description: 'Special',
+        category: 'Demonlord',
+        foreground: '#f2f2f2',
+        background: '#6F0000',
+        outline: '#651320',
+        edge: '#020202',
+        texture: 'marble',
+        default: true
+    });
+});
+
 /* -------------------------------------------- */
 /*  Hotbar Macros                               */
 /* -------------------------------------------- */
