3cbefb1bc7c06382d6645e81a2b8ad77e3bd6082
Patrick Porto <patrick.porto@loft.com.br>
2021-04-27 23:40:53 -0300

    refactor: change controlled to targets

--- src/module/item/item.js
+++ src/module/item/item.js
@@ -41,7 +41,7 @@ export class DemonlordItem extends Item {
     const item = li.children[0];
     const healing = parseInt(item.dataset.healing);
 
-    var selected = canvas.tokens.controlled;
+    var selected = Array.from(game.user.targets);
     if (selected.length == 0) {
       ui.notifications.info(game.i18n.localize('DL.DialogWarningActorsNotSelected'));
     }
@@ -188,7 +188,7 @@ export class DemonlordItem extends Item {
     const item = li.children[0];
     const damage = parseInt(item.dataset.damage);
 
-    var selected = canvas.tokens.controlled;
+    var selected = Array.from(game.user.targets);
     if (selected.length == 0) {
       ui.notifications.info(game.i18n.localize('DL.DialogWarningActorsNotSelected'));
     }
@@ -271,7 +271,7 @@ export class DemonlordItem extends Item {
     if (boonsbanes == undefined) boonsbanes = parseInt(item.dataset.boba);
     if (isNaN(boonsbanes)) boonsbanes = 0;
 
-    var selected = canvas.tokens.controlled;
+    var selected = Array.from(game.user.targets);
     if (selected.length == 0) {
       ui.notifications.info(game.i18n.localize('DL.DialogWarningActorsNotSelected'));
     }
