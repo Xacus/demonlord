56829c28a0e6eeb9ffdbc3daef45a7ba7910f977
Bo Kehlet <faktor3@gmail.com>
2021-06-01 00:38:40 +0200

    Release 2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ const { nodeResolve } = require('@rollup/plugin-node-resolve')
 module.exports = {
   input: 'src/module/demonlord.js',
   output: {
-    dir: '/Users/bokehlet/Library/Application Support/FoundryVTT/Data/systems/demonlord/module',
+    dir: 'dist/module',
     format: 'es',
     sourcemap: true,
   },
