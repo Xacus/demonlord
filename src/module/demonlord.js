1fe1c41cba863a15fa9c723b0243b08a616c0fc9
Patrick Porto <patrick.s.porto@gmail.com>
2021-06-05 15:52:38 -0300

    refactor: extract action template to pixi module

--- src/module/demonlord.js
+++ src/module/demonlord.js
@@ -2,7 +2,7 @@
 import { DL } from './config.js'
 import { DemonlordActor } from './actor/actor.js'
 import { DemonlordItem } from './item/item.js'
-import { ActionTemplate } from './item/action-template.js'
+import { ActionTemplate } from './pixi/action-template.js'
 import { registerSettings } from './settings.js'
 import { nextTurn, rollInitiative, setupTurns, startCombat } from './init/init.js'
 import combattracker, { _onUpdateCombat } from './combattracker.js'
