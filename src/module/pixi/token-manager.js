3fe534e95670c73e8086d6049cbf65c29a007f88
Patrick Porto <patrick.s.porto@gmail.com>
2021-06-06 15:01:05 -0300

    feat: add select objects support on Measured Template

--- src/module/pixi/token-manager.js
+++ src/module/pixi/token-manager.js
@@ -6,6 +6,17 @@ export class TokenManager {
     return [...game.user.targets]
   }
 
+  targetTokens(tokens) {
+    if (game.settings.get('demonlord', 'targetingOnSelect')) {
+      for (let tokenId of tokens) {
+        const token = canvas.tokens.placeables.find(t => t.id === tokenId)
+        token.control({releaseOthers: false})
+      }
+    } else {
+      game.user.updateTokenTargets(tokens)
+    }
+  }
+
   getTokenByActorId(actorId) {
     return canvas.tokens.placeables.find(token => token.actor.id === actorId)
   }
