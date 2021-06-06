1b2b0bad551697f2942b2b605538ca2ee9de0c25
ClipplerBlood <pqmanuelpq@gmail.com>
2021-06-06 11:43:43 +0200

    Converted Ancestry Sheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-const { rollup } = require('rollup');
-const argv = require('yargs').argv;
-const chalk = require('chalk');
-const fs = require('fs-extra');
-const gulp = require('gulp');
-const path = require('path');
-const rollupConfig = require('./rollup.config');
-const semver = require('semver');
-const sass = require('gulp-sass');
+const { rollup } = require('rollup')
+const argv = require('yargs').argv
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const gulp = require('gulp')
+const path = require('path')
+const rollupConfig = require('./rollup.config')
+const semver = require('semver')
+const sass = require('gulp-sass')
 const pug = require('gulp-pug')
-sass.compiler = require('sass');
+sass.compiler = require('sass')
 
 /********************/
 /*  CONFIGURATION   */
 /********************/
 
-const name = path.basename(path.resolve('.'));
-const sourceDirectory = './src';
-const distDirectory = './dist';
-const pugDirectory = './src/templates'
-const stylesDirectory = `${sourceDirectory}/styles`;
-const stylesExtension = 'scss';
-const sourceFileExtension = 'js';
-const staticFiles = ['assets', 'fonts', 'lang', 'packs', 'templates', 'system.json', 'template.json', 'lib']; //FIXME: remove templates here, after conversion is complete
-const getDownloadURL = (version) => `https://host/path/to/${version}.zip`;
+const name = path.basename(path.resolve('.'))
+const sourceDirectory = './src'
+const distDirectory = './dist'
+const pugDirectory = 'src/templates'
+const stylesDirectory = `${sourceDirectory}/styles`
+const stylesExtension = 'scss'
+const sourceFileExtension = 'js'
+const staticFiles = ['assets', 'fonts', 'lang', 'packs', 'templates', 'system.json', 'template.json', 'lib'] //FIXME: remove templates here, after conversion is complete
+const getDownloadURL = version => `https://host/path/to/${version}.zip`
 
 /********************/
 /*      BUILD       */
@@ -32,8 +32,8 @@ const getDownloadURL = (version) => `https://host/path/to/${version}.zip`;
  * Build the distributable JavaScript code
  */
 async function buildCode() {
-  const build = await rollup({ input: rollupConfig.input, plugins: rollupConfig.plugins });
-  return build.write(rollupConfig.output);
+  const build = await rollup({ input: rollupConfig.input, plugins: rollupConfig.plugins })
+  return build.write(rollupConfig.output)
 }
 
 /**
@@ -43,7 +43,7 @@ function buildStyles() {
   return gulp
     .src(`${stylesDirectory}/${name}.${stylesExtension}`)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(`${distDirectory}/styles`));
+    .pipe(gulp.dest(`${distDirectory}/styles`))
 }
 
 /**
@@ -52,7 +52,7 @@ function buildStyles() {
 async function copyFiles() {
   for (const file of staticFiles) {
     if (fs.existsSync(`${sourceDirectory}/${file}`)) {
-      await fs.copy(`${sourceDirectory}/${file}`, `${distDirectory}/${file}`);
+      await fs.copy(`${sourceDirectory}/${file}`, `${distDirectory}/${file}`)
     }
   }
 }
@@ -61,14 +61,14 @@ async function copyFiles() {
  * Watch for changes for each build step
  */
 function buildWatch() {
-  gulp.watch(`${sourceDirectory}/**/*.${sourceFileExtension}`, { ignoreInitial: false }, buildCode);
-  gulp.watch(`${stylesDirectory}/**/*.${stylesExtension}`, { ignoreInitial: false }, buildStyles);
-  gulp.watch(`${pugDirectory}/**/*.pug`, { ignoreInitial: false }, buildPug);
+  gulp.watch(`${sourceDirectory}/**/*.${sourceFileExtension}`, { ignoreInitial: false }, buildCode)
+  gulp.watch(`${stylesDirectory}/**/*.${stylesExtension}`, { ignoreInitial: false }, buildStyles)
+  gulp.watch(`${pugDirectory}/**/*.pug`, { ignoreInitial: false }, buildPug)
   gulp.watch(
-    staticFiles.map((file) => `${sourceDirectory}/${file}`),
+    staticFiles.map(file => `${sourceDirectory}/${file}`),
     { ignoreInitial: false },
     copyFiles,
-  );
+  )
 }
 
 /********************/
@@ -79,17 +79,17 @@ function buildWatch() {
  * Remove built files from `dist` folder while ignoring source files
  */
 async function clean() {
-  const files = [...staticFiles, 'module'];
+  const files = [...staticFiles, 'module']
 
   if (fs.existsSync(`${stylesDirectory}/${name}.${stylesExtension}`)) {
-    files.push('styles');
+    files.push('styles')
   }
 
-  console.log(' ', chalk.yellow('Files to clean:'));
-  console.log('   ', chalk.blueBright(files.join('\n    ')));
+  console.log(' ', chalk.yellow('Files to clean:'))
+  console.log('   ', chalk.blueBright(files.join('\n    ')))
 
   for (const filePath of files) {
-    await fs.remove(`${distDirectory}/${filePath}`);
+    await fs.remove(`${distDirectory}/${filePath}`)
   }
 }
 
@@ -101,16 +101,16 @@ async function clean() {
  * Get the data path of Foundry VTT based on what is configured in `foundryconfig.json`
  */
 function getDataPath() {
-  const config = fs.readJSONSync('foundryconfig.json');
+  const config = fs.readJSONSync('foundryconfig.json')
 
   if (config?.dataPath) {
     if (!fs.existsSync(path.resolve(config.dataPath))) {
-      throw new Error('User Data path invalid, no Data directory found');
+      throw new Error('User Data path invalid, no Data directory found')
     }
 
-    return path.resolve(config.dataPath);
+    return path.resolve(config.dataPath)
   } else {
-    throw new Error('No User Data path defined in foundryconfig.json');
+    throw new Error('No User Data path defined in foundryconfig.json')
   }
 }
 
@@ -118,23 +118,23 @@ function getDataPath() {
  * Link build to User Data folder
  */
 async function linkUserData() {
-  let destinationDirectory;
+  let destinationDirectory
   if (fs.existsSync(path.resolve(sourceDirectory, 'system.json'))) {
-    destinationDirectory = 'systems';
+    destinationDirectory = 'systems'
   } else {
-    throw new Error(`Could not find ${chalk.blueBright('system.json')}`);
+    throw new Error(`Could not find ${chalk.blueBright('system.json')}`)
   }
 
-  const linkDirectory = path.resolve(getDataPath(), destinationDirectory, name);
+  const linkDirectory = path.resolve(getDataPath(), destinationDirectory, name)
 
   if (argv.clean || argv.c) {
-    console.log(chalk.yellow(`Removing build in ${chalk.blueBright(linkDirectory)}.`));
+    console.log(chalk.yellow(`Removing build in ${chalk.blueBright(linkDirectory)}.`))
 
-    await fs.remove(linkDirectory);
+    await fs.remove(linkDirectory)
   } else if (!fs.existsSync(linkDirectory)) {
-    console.log(chalk.green(`Linking dist to ${chalk.blueBright(linkDirectory)}.`));
-    await fs.ensureDir(path.resolve(linkDirectory, '..'));
-    await fs.symlink(path.resolve(distDirectory), linkDirectory);
+    console.log(chalk.green(`Linking dist to ${chalk.blueBright(linkDirectory)}.`))
+    await fs.ensureDir(path.resolve(linkDirectory, '..'))
+    await fs.symlink(path.resolve(distDirectory), linkDirectory)
   }
 }
 
@@ -146,13 +146,13 @@ async function linkUserData() {
  * Get the contents of the manifest file as object.
  */
 function getManifest() {
-  const manifestPath = `${sourceDirectory}/system.json`;
+  const manifestPath = `${sourceDirectory}/system.json`
 
   if (fs.existsSync(manifestPath)) {
     return {
       file: fs.readJSONSync(manifestPath),
       name: 'system.json',
-    };
+    }
   }
 }
 
@@ -161,9 +161,9 @@ function getManifest() {
  */
 function getTargetVersion(currentVersion, release) {
   if (['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease'].includes(release)) {
-    return semver.inc(currentVersion, release);
+    return semver.inc(currentVersion, release)
   } else {
-    return semver.valid(release);
+    return semver.valid(release)
   }
 }
 
@@ -171,48 +171,48 @@ function getTargetVersion(currentVersion, release) {
  * Update version and download URL.
  */
 function bumpVersion(cb) {
-  const packageJson = fs.readJSONSync('package.json');
-  const packageLockJson = fs.existsSync('package-lock.json') ? fs.readJSONSync('package-lock.json') : undefined;
-  const manifest = getManifest();
+  const packageJson = fs.readJSONSync('package.json')
+  const packageLockJson = fs.existsSync('package-lock.json') ? fs.readJSONSync('package-lock.json') : undefined
+  const manifest = getManifest()
 
-  if (!manifest) cb(Error(chalk.red('Manifest JSON not found')));
+  if (!manifest) cb(Error(chalk.red('Manifest JSON not found')))
 
   try {
-    const release = argv.release || argv.r;
+    const release = argv.release || argv.r
 
-    const currentVersion = packageJson.version;
+    const currentVersion = packageJson.version
 
     if (!release) {
-      return cb(Error('Missing release type'));
+      return cb(Error('Missing release type'))
     }
 
-    const targetVersion = getTargetVersion(currentVersion, release);
+    const targetVersion = getTargetVersion(currentVersion, release)
 
     if (!targetVersion) {
-      return cb(new Error(chalk.red('Error: Incorrect version arguments')));
+      return cb(new Error(chalk.red('Error: Incorrect version arguments')))
     }
 
     if (targetVersion === currentVersion) {
-      return cb(new Error(chalk.red('Error: Target version is identical to current version')));
+      return cb(new Error(chalk.red('Error: Target version is identical to current version')))
     }
 
-    console.log(`Updating version number to '${targetVersion}'`);
+    console.log(`Updating version number to '${targetVersion}'`)
 
-    packageJson.version = targetVersion;
-    fs.writeJSONSync('package.json', packageJson, { spaces: 2 });
+    packageJson.version = targetVersion
+    fs.writeJSONSync('package.json', packageJson, { spaces: 2 })
 
     if (packageLockJson) {
-      packageLockJson.version = targetVersion;
-      fs.writeJSONSync('package-lock.json', packageLockJson, { spaces: 2 });
+      packageLockJson.version = targetVersion
+      fs.writeJSONSync('package-lock.json', packageLockJson, { spaces: 2 })
     }
 
-    manifest.file.version = targetVersion;
-    manifest.file.download = getDownloadURL(targetVersion);
-    fs.writeJSONSync(`${sourceDirectory}/${manifest.name}`, manifest.file, { spaces: 2 });
+    manifest.file.version = targetVersion
+    manifest.file.download = getDownloadURL(targetVersion)
+    fs.writeJSONSync(`${sourceDirectory}/${manifest.name}`, manifest.file, { spaces: 2 })
 
-    return cb();
+    return cb()
   } catch (err) {
-    cb(err);
+    cb(err)
   }
 }
 
@@ -220,11 +220,11 @@ function bumpVersion(cb) {
  * Set download url based on version
  */
 function setDownloadURL(cb) {
-  const systemJson = fs.readJSONSync('src/system.json');
+  const systemJson = fs.readJSONSync('src/system.json')
   const version = systemJson.version
   const newDownloadURL = `https://github.com/ClipplerBlood/demonlord-cromethius/releases/download/${version}/system.zip`
   systemJson.download = newDownloadURL
-  console.info("Updating system download URL to " + newDownloadURL + '\n')
+  console.info('Updating system download URL to ' + newDownloadURL + '\n')
   cb()
 }
 /********************/
@@ -232,9 +232,10 @@ function setDownloadURL(cb) {
 /********************/
 
 function buildPug(cb) {
-  gulp.src('src/templates/**/*.pug')
-    .pipe(pug({basedir: 'src/templates'}))
-    .pipe(gulp.dest('dist/templates/'));
+  gulp
+    .src('src/templates/**/*.pug')
+    .pipe(pug({ basedir: 'src/templates', doctype: 'html' }))
+    .pipe(gulp.dest('dist/templates/'))
   cb()
 }
 
@@ -242,12 +243,12 @@ function buildPug(cb) {
 /*      EXPORTS     */
 /********************/
 
-const execBuild = gulp.parallel(buildCode, buildStyles, copyFiles, buildPug);
+const execBuild = gulp.parallel(buildCode, buildStyles, copyFiles, buildPug)
 
-exports.build = gulp.series(clean, execBuild);
-exports.watch = buildWatch;
-exports.clean = clean;
-exports.link = linkUserData;
-exports.bumpVersion = bumpVersion;
-exports.setDownloadURL = setDownloadURL;
-exports.pug = buildPug;
+exports.build = gulp.series(clean, execBuild)
+exports.watch = buildWatch
+exports.clean = clean
+exports.link = linkUserData
+exports.bumpVersion = bumpVersion
+exports.setDownloadURL = setDownloadURL
+exports.pug = buildPug
