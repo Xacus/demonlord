0909abc70cc7bc12792f812e6770562da35641d6
juanferrer <juanemilioferrer@outlook.com>
2026-05-22 13:07:38 +0100

    WIP: Migrate from measured templates to scene regions

--- src/module/pixi/action-template.js
+++ src/module/pixi/action-template.js
@@ -60,6 +60,9 @@ export class ActionTemplate extends foundry.canvas.placeables.Region {
       case 'circle':
         templateData.radius = value
         break
+      case 'circle':
+        template.radius = value
+        break
       default:
         break
     }
